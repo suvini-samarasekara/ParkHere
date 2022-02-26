0491bb5fe3c71154f04f86c33c8be89e2dc462d5
suvini-samarasekara <suvinikaveesha@gmail.com>
2022-02-26 17:16:51 +0530

    Project Initialize, psql connection establish and check data pass or not

---
+++ ParkHere/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Npgsql;
+
+
+namespace ParkHere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //TestConnection();
+            Console.WriteLine("Enter Vehicle Number : ");
+            string vehicleNumber = Console.ReadLine();
+            Console.WriteLine("Enter Your NIC : ");
+            string ownerNIC = Console.ReadLine();
+            InsertRecord(vehicleNumber, ownerNIC);
+            Console.ReadKey();
+        }
+
+        private static void InsertRecord(string vehicleNumber,string ownerNIC)
+        {
+            using(NpgsqlConnection con = GetConnection())
+            {
+                string query = $@"INSERT INTO public.Vehicle(VehicleNumber,OwnerNIC)VALUES(@vehicleNumber,@ownerNIC)";
+                
+                NpgsqlCommand cmd = new NpgsqlCommand(query,con);
+                con.Open();
+
+                cmd.Parameters.AddWithValue("vehicleNumber", vehicleNumber);
+                cmd.Parameters.AddWithValue("ownerNIC", ownerNIC);
+                cmd.Prepare();
+                int n = cmd.ExecuteNonQuery();
+                if (n == 1)
+                    Console.WriteLine("Inserted");
+            }
+        }
+
+        private static void TestConnection()
+        {
+            using(NpgsqlConnection con = GetConnection())
+            {
+                con.Open();
+                if (con.State == System.Data.ConnectionState.Open)
+                    Console.WriteLine("Connected");
+            }
+        }
+
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=suvini;Database=ParkHereDB;");
+        }
+
+    }
+}
